e7185fe96164c9cd481e89bd6e5ce1dae0698b5a
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2024-06-10 10:29:26 +0500

    Update vite.config.js
    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://my-portfolio-backend-nilc.onrender.com', // Update with your backend server URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // Remove '/api' prefix
+      }
+    }
+  }
 });
